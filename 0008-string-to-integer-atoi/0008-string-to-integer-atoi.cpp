03664819a881fc8ae1999eb0c222e53c4873a020
Lovish Goyal <goyallovish852@gmail.com>
2026-04-15 16:55:57 +0530

    Time: 0 ms (100%), Space: 9 MB (97.87%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    int sign = 1;
+
+    long long rec(string &s, int i, long long ans, bool started) {
+        if (i >= s.size()) return ans;
+
+        // skip leading spaces
+        if (!started && s[i] == ' ')
+            return rec(s, i + 1, ans, false);
+
+        // sign handling
+        if (!started && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            return rec(s, i + 1, ans, true);
+        }
+
+        // digit processing
+        if (isdigit(s[i])) {
+            started = true;
+            ans = ans * 10 + (s[i] - '0');
+
+            // overflow check
+            if (sign == 1 && ans > INT_MAX) return INT_MAX;
+            if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+
+            return rec(s, i + 1, ans, true);
+        }
+
+        // stop if non-digit
+        return ans;
+    }
+
     int myAtoi(string s) {
-      int i=0;
-      while(i < s.size() && s[i] == ' ') i++;
-
-      int sign=1;
-      if(s[i]=='+' || s[i]=='-'){
-        if(s[i]=='-') sign=-1;
-        i++;
-      }
-      long result=0;
-      while(s[i]>='0' && s[i]<='9'){
-        result=result*10 + (s[i]-'0');
-
-        if(result*sign>INT_MAX) return  INT_MAX;
-        if(result*sign<INT_MIN) return  INT_MIN;
-
-        i++;
-      }
-      return sign*result;
+        long long ans = rec(s, 0, 0, false);
+        return sign * ans;
     }
 };
