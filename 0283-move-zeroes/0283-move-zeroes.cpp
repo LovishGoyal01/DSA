a431ec2298677384ee63135ddfde4fc24b67b2d0
Lovish Goyal <goyallovish852@gmail.com>
2026-05-30 11:34:35 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int n=arr.size();
-        int j=-1;
-        for(int i=0;i<n;i++){
-            if(j==-1 && arr[i]==0){
-                j=i;
-            }
-            else if(j!=-1 && arr[i]!=0){
-                arr[j]=arr[i];
-                j++;
-                arr[i]=0;
+        int i=0,j;
+        for(int j=0;j<arr.size();j++){
+            if(arr[j]!=0){
+              swap(arr[i],arr[j]);
+              i++;
             }
         }
-       
     }
 };
