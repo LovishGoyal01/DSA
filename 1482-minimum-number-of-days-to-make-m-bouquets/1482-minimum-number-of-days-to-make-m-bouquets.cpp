e79ed833e30569b3e23348c9d1847b5103fef25b
Lovish Goyal <goyallovish852@gmail.com>
2026-05-31 20:09:01 +0530

    Time: 19 ms (82.14%), Space: 70.5 MB (76.82%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
-    
-    int possible(vector<int>& bloomDay, int mid, int k, int m){
-        int cnt = 0, noB = 0;
-
-        for(int i = 0; i < bloomDay.size(); i++){
-            if(bloomDay[i] <= mid){
-                cnt++;
-            } else {
-                noB += cnt / k;
-                cnt = 0;
+    int calc(vector<int>& arr,int mid, int m, int k){
+        int ans=0,count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=mid) count++;
+            else{
+                ans+=count/k;
+                count=0;
             }
+            
         }
-
-        noB += cnt / k;
-
-        return noB >= m;
+        ans+=count/k;
+        if(ans>=m) return 1;
+        return 0;
     }
 
-    vector<int> mini(vector<int>& bloomDay){
-        int maxv = INT_MIN;
-        int minv = INT_MAX;
-
-        for(int i = 0; i < bloomDay.size(); i++){
-            maxv = max(maxv, bloomDay[i]);
-            minv = min(minv, bloomDay[i]);
-        }
-        return {minv, maxv};
-    }
-
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-
-        if(n < (long long)m * k) return -1;
-
-        auto it = mini(bloomDay);
-
-        int low = it[0];
-        int high = it[1];
-        int ans = high;
-
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-
-            if(possible(bloomDay, mid, k, m)){
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    int minDays(vector<int>& arr, int m, int k) {
+        int low=1;
+        int high=*max_element(arr.begin(),arr.end());
+        int mid,ans=-1;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(calc(arr,mid,m,k)==1){
+                ans=mid;
+                high=mid-1;
+            }else low=mid+1;
         }
         return ans;
     }
