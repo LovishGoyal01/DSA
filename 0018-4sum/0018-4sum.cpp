25427e3b68f6ebd6d77ee9675f14e997d0c485ad
Lovish Goyal <goyallovish852@gmail.com>
2026-05-31 10:37:55 +0530

    Time: 23 ms (54.59%), Space: 17.4 MB (81.81%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;j++){
-              if(j>i+1 && nums[j]==nums[j-1]) continue;   
-              int k=j+1;
-              int l=n-1;
-              while(k<l){
-                long long sum =nums[i];
-                sum += nums[j] ;
-                sum += nums[k] ;
-                sum += nums[l] ;
-                if(sum<target){
-                    k++;
-                }else if(sum>target){
-                    l--;
-                }else{
-                    ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                    k++;
-                    l--;
-                    while(k<l && nums[k]==nums[k-1]) k++;
-                    while(k<l && nums[l]==nums[l+1]) l--;
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        int i,j,k,l;
+        long long sum;
+
+        vector<vector<int>> ans;
+        
+        for(i=0;i<n-3;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && arr[j]==arr[j-1]) continue;
+                k=j+1;
+                l=n-1;
+                while(k<l){
+                    sum=(long long)arr[i]+arr[j]+arr[k]+ arr[l];
+                    if(sum>target) l--;
+                    else if(sum<target) k++;
+                    else {
+                        ans.push_back({arr[i],arr[j],arr[k],arr[l]});
+                        k++;
+                        l--;
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(k<l && arr[l]==arr[l+1]) l--;    
+                    }
                 }
-              }
             }
         }
-         return ans;
+        return ans;
     }
 };
