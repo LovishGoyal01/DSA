bfe047d8e4a100298913fa8a0608e9049aac3075
Lovish Goyal <goyallovish852@gmail.com>
2026-05-31 23:09:40 +0530

    Time: 9 ms (45.91%), Space: 26.2 MB (14.54%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-
-    int possible(vector<int>& nums, int threshold,int mid){
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-          cnt+=ceil((double)nums[i] / mid);
+    int calc(vector<int>& arr,int mid, int t){
+        int total=0;
+        for(int i=0;i<arr.size();i++){
+            total+=ceil((double)arr[i]/mid);
         }
-        if(cnt<=threshold) return 1;
+        if(total<=t) return 1;
         return 0;
     }
 
-    int smallestDivisor(vector<int>& nums, int threshold) {
-         int n= nums.size();
-         int low= 1;
-         int high=*max_element(nums.begin(),nums.end());
-         if(n==threshold) return high;
-         int ans= high;
-         int mid=0;
-         while(low<=high){
-            mid=(low+high)/2;
-            if(possible(nums,threshold,mid)){
-             ans=mid;   
-             high=mid-1;
-            }else low=mid+1;
-         }
-         return ans;
+    int smallestDivisor(vector<int>& arr, int t) {
+      int n=arr.size();  
+      int low=1;
+      int high=*max_element(arr.begin(),arr.end());
+      int mid,ans=-1;
+      while(low<=high){
+        mid=(low+high)/2;
+        if(calc(arr,mid,t)==1){
+            high=mid-1;
+            ans=mid;
+        }else low=mid+1;
+      }
+      return ans;
     }
 };
