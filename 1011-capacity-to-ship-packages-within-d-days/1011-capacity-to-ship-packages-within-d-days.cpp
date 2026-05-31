8ad4a0bd4a1445c8d0c11ef91571ab98af30678c
Lovish Goyal <goyallovish852@gmail.com>
2026-05-31 23:33:53 +0530

    Time: 10 ms (72.12%), Space: 35.1 MB (18.69%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    vector<int> total(vector<int>& weights){
-        int sum=0,maxi=0;
-        for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
-            maxi=max(maxi,weights[i]);
+    int calc(vector<int>& arr,int mid, int days){
+        int total=0,count=1;
+        for(int i=0;i<arr.size();i++){
+            if(total+arr[i]>mid){
+                count++;
+                total=arr[i];
+            }else total+=arr[i];
         }
-        return {maxi,sum};
-    }
-
-    int possible(vector<int>& weights, int days,int mid){
-        int cnt=0,sum=0;
-        for(int i=0;i<weights.size();i++){
-          sum=sum+weights[i];
-          if(ceil((double)sum / mid)==2){
-            cnt++;
-            sum=weights[i];
-          }
-        }
-        if(cnt+1<=days) return 1;
+        if(count<=days) return 1;
         return 0;
     }
 
-    int shipWithinDays(vector<int>& weights, int days) {
-       auto it = total(weights);
-       int low = it[0];
-       int high = it[1];
-       int mid=0,ans=high;
-       while(low<=high){
-         mid = (low+high)/2;
-         if(possible(weights,days,mid)){
-            ans=mid;   
-             high=mid-1;
-        }else low=mid+1;
-       }
-     return ans; 
+    int shipWithinDays(vector<int>& arr, int days) {
+        int n=arr.size();
+        int low = *max_element(arr.begin(), arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
+        int mid,ans=-1;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(calc(arr,mid,days)==1){
+                high=mid-1;
+                ans=mid;
+            }else low=mid+1;
+        }
+        return ans;
     }
 };
