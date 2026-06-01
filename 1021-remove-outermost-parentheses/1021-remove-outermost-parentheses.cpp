cae61734235e4250567071d11960e41988eb280d
Lovish Goyal <goyallovish852@gmail.com>
2026-06-01 22:19:07 +0530

    Time: 0 ms (100%), Space: 9 MB (53.87%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
-        int cnt=0;
-        for(auto it:s){
-            if(it=='('){
-             cnt++;
-             if(cnt>1) ans+=it;
-            } 
-            else{
-                if(cnt>1) ans+=it;
-                cnt--; 
-            }
-            
-            
-        }
-        return ans;
+       string st="";
+       int count=0;
+       for(char ch:s){
+         if(ch=='('){
+            count++;
+            if(count==1) continue;
+            else st+=ch; 
+         }else if(ch==')'){
+            count--;
+            if(count==0) continue;
+            else st+=ch;
+         }
+       }
+       return st; 
     }
 };
