36bd926d591c8ecf9cc5ad74db924c900eede8c9
Lovish Goyal <goyallovish852@gmail.com>
2026-06-01 11:38:15 +0530

    Time: 0 ms (100%), Space: 10.5 MB (72.44%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     
-    int possible(vector<int>& nums, int k,int mid){
-        int sum=0,cnt=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(sum> mid){
-                cnt++;
-                sum=nums[i];
+    int possible(vector<int>& arr, int mid,int k){
+        int painter=1,total=0;
+        for(int i=0;i<arr.size();i++){
+            if(total+arr[i]<=mid) total+=arr[i];
+            else{
+                painter++;
+                total=arr[i];
             }
         }
-        if(cnt+1<=k) return 1;
-        return 0; 
+        if(painter<=k) return 1;
+        return 0;
     }
 
     int splitArray(vector<int>& nums, int k) {
@@ -20,11 +20,11 @@ public:
         int high=accumulate(nums.begin(),nums.end(),0);
         int mid=0,ans=high;
         while(low<=high){
-            mid=(low+high)/2;
-            if(possible(nums,k,mid)){
-                high=mid-1;
-                ans=mid;
-            }else low=mid+1;
+          mid=(low+high)/2;
+          if(possible(nums,mid,k)){
+            ans=mid;
+            high=mid-1;
+          }else low=mid+1;
         }
         return ans;
     }
