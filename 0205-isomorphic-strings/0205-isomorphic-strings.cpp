cbcf656faa347852159c4417d034d47960768041
Lovish Goyal <goyallovish852@gmail.com>
2026-06-02 11:19:45 +0530

    Time: 0 ms (100%), Space: 9.6 MB (7.73%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,7 +4,7 @@ public:
         int n= s.size();
         int m=t.size();
         if(n!=m) return false;
-       map<char,char>mp1,mp2;
+       unordered_map<char,char>mp1,mp2;
 
        for(int i=0;i<n;i++){
         if(mp1.find(s[i])!=mp1.end() && mp1[s[i]]!=t[i]) return false;  
