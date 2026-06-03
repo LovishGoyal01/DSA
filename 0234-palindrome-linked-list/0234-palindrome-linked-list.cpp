dc88f87e4c68ae3b44ebeb77b55ed4ddc146449f
Lovish Goyal <goyallovish852@gmail.com>
2026-06-03 13:26:15 +0530

    Time: 1 ms (67.34%), Space: 122.1 MB (39.28%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -7,47 +7,39 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
-
  */
 class Solution {
 public:
-    
-    ListNode* reverse(ListNode *head){
-        if(head==nullptr || head->next==nullptr) return head;
-
-        ListNode* curr = head;
+    ListNode* reverse(ListNode* node){
         ListNode* prev=nullptr;
         ListNode* temp=nullptr;
-        while(curr!=nullptr){
-            temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
+        while(node!=nullptr){
+            temp=node->next;
+            node->next=prev;
+            prev=node;
+            node=temp;
         }
         return prev;
     }
-
     bool isPalindrome(ListNode* head) {
-      ListNode* fast=head;  
-      ListNode* slow=head;
-      while(fast->next!=nullptr && fast->next->next!=nullptr){
-        slow=slow->next;
-        fast = fast->next->next;
-      }
-      ListNode* newhead= reverse(slow->next);
-      ListNode* first=head;  
-      ListNode* second=newhead;
-
-      while(second!=nullptr){
-        if(first->val!=second->val){
-            reverse(newhead);
-            return false;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        first=first->next;
-        second=second->next;
-      }
-      reverse(newhead);
-      return true;
-
+        ListNode* newHead=reverse(slow->next);
+        ListNode* first=head;
+        ListNode* second=newHead;
+        while(second!=nullptr){
+            if(second->val!=first->val){
+                reverse(newHead);
+                 return false;
+            }
+            second=second->next;
+            first=first->next;
+        }
+        reverse(newHead);
+        return true;
     }
 };
