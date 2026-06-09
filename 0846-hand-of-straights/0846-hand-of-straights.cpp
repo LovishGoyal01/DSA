c496f8a5336abda4cc97adbe4f0131a44778fccc
Lovish Goyal <goyallovish852@gmail.com>
2026-06-09 17:29:02 +0530

    Time: 44 ms (29.08%), Space: 32.3 MB (60.35%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) return false;
+
+        map<int,int> mpp;
+
+        for(int x : hand)
+            mpp[x]++;
+
+        while(!mpp.empty()) {
+            int start = mpp.begin()->first;
+
+            for(int i = 0; i < groupSize; i++) {
+                int card = start + i;
+
+                if(mpp.find(card) == mpp.end())
+                    return false;
+
+                mpp[card]--;
+
+                if(mpp[card] == 0)
+                    mpp.erase(card);
+            }
+        }
+
+        return true;
+    }
+};
