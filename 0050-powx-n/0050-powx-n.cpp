d7f34ef1db412cb265a06699623e19daf9b69388
Lovish Goyal <goyallovish852@gmail.com>
2026-06-14 19:16:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       long long N = n;
-       if(n>0) return  myPowA(x,N);
-       return myPowA(1/x,-N);
-    }
-    double myPowA(double x, int n) {
-       if(n==0) return  1.0;
-       double half=myPowA(x,n/2);
-       if(n%2==0) return half*half;
-       return half*half*x; 
+        long long N = n;
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= x;
+                N--;
+            } else {
+                x *= x;
+                N /= 2;
+            }
+        }
+
+        return ans;
     }
 };
