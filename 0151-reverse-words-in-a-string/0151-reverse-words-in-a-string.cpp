18e278341d9be5f3ec43219e075bc9d3ffa69ce8
Lovish Goyal <goyallovish852@gmail.com>
2026-06-18 22:36:33 +0530

    Time: 0 ms (100%), Space: 10.4 MB (52.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,9 +6,10 @@ public:
         for(int i=0;i<s.size();i++){
             string word="";
             while(i<s.size() && s[i]!=' '){
-                word=s[i]+word;
+                word=word+s[i];
                 i++;
             }
+            reverse(word.begin(),word.end());
             if(word.size()>0) ans+=" "+word;
         }
         return ans.substr(1);
