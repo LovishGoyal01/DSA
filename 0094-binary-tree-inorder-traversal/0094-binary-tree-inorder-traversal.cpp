650bdcbbd7651dc559cb664b27901b28eb2cc98d
Lovish Goyal <goyallovish852@gmail.com>
2026-06-21 22:24:35 +0530

    Time: 0 ms (100%), Space: 10.9 MB (65.74%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,51 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root,vector<int>& ans){
-        if(root==nullptr) return;
-        traverse(root->left,ans);
-        ans.push_back(root->val);
-        traverse(root->right,ans);
-    }
+    // void traverse(TreeNode* root,vector<int>& ans){
+    //     if(root==nullptr) return;
+    //     traverse(root->left,ans);
+    //     ans.push_back(root->val);
+    //     traverse(root->right,ans);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //    vector<int> ans;
+    //     traverse(root,ans);
+    //     return ans;   
+    // }
+
     vector<int> inorderTraversal(TreeNode* root) {
-       vector<int> ans;
-        traverse(root,ans);
-        return ans;   
+            vector<int> inorder;
+        TreeNode* cur = root;
+
+        while (cur != NULL) {
+
+            // Case 1: No left child
+            if (cur->left == NULL) {
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }
+            // Case 2: Left child exists
+            else {
+                TreeNode* prev = cur->left;
+
+                // Find inorder predecessor
+                while (prev->right != NULL && prev->right != cur) {
+                    prev = prev->right;
+                }
+
+                // Create thread
+                if (prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                // Remove thread
+                else {
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
