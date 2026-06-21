df1f413bf3450ffb0807ef1f73901c916f0d940a
Lovish Goyal <goyallovish852@gmail.com>
2026-06-21 16:03:36 +0530

    Time: 0 ms (100%), Space: 37.3 MB (47.97%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -6,31 +6,70 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class BSTIterator {
+    stack<TreeNode*> myStack;
+    bool reverse;
+
 public:
-    bool findval(TreeNode* root,int valu,int required){
-       while(root){
-        if(root->val!=valu && root->val==required) return true;
-        else if(root->val<required) root=root->right;
-        else root=root->left;
-       }
-       return false;
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
     }
 
-    bool findTarget(TreeNode* root, int k) {
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-          TreeNode* node = st.top();
-          st.pop();
-          bool ans = findval(root,node->val,k-node->val);
-          if(ans) return true;
-          if(node->left) st.push(node->left);
-          if(node->right) st.push(node->right); 
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    int next() {
+        TreeNode* tmpNode = myStack.top();
+        myStack.pop();
+
+        if (!reverse)
+            pushAll(tmpNode->right);
+        else
+            pushAll(tmpNode->left);
+
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode* node) {
+        while (node) {
+            myStack.push(node);
+
+            if (reverse)
+                node = node->right;
+            else
+                node = node->left;
         }
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+
+        BSTIterator l(root, false); // inorder
+        BSTIterator r(root, true);  // reverse inorder
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j) {
+            if (i + j == k)
+                return true;
+            else if (i + j < k)
+                i = l.next();
+            else
+                j = r.next();
+        }
+
         return false;
     }
 };
