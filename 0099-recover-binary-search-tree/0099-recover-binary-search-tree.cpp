fee051c2e77701bd6ca602999a8b8518a2696baf
Lovish Goyal <goyallovish852@gmail.com>
2026-06-21 17:11:38 +0530

    Time: 9 ms (5.38%), Space: 60.9 MB (51.33%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -6,39 +6,53 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 private:
-  TreeNode* first;
-  TreeNode* middle;
-  TreeNode* last;
-  TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+    TreeNode* prev;
 
-  void inorder(TreeNode* root){
-    if(root==nullptr) return;
+    void inorder(TreeNode* root) {
+        if (root == nullptr) return;
 
-    inorder(root->left);
+        inorder(root->left);
 
-    if(prev && root->val<prev->val){
-        if(first){
-            last = root;
-        }else{
-            first = prev;
-            middle = root;          
+        if (prev != nullptr && root->val < prev->val) {
+
+            // First violation
+            if (first == nullptr) {
+                first = prev;
+                middle = root;
+            }
+            // Second violation
+            else {
+                last = root;
+            }
         }
+
+        prev = root;
+
+        inorder(root->right);
     }
-    prev = root;
-    inorder(root->right);
-  }
 
 public:
     void recoverTree(TreeNode* root) {
-       first = middle = last =nullptr;
-       prev = new TreeNode(INT_MIN);
-       inorder(root);
-       if(first && last) swap(first->val,last->val);
-       else if(first && middle) swap(first->val,middle->val); 
+        first = middle = last = prev = nullptr;
+
+        inorder(root);
+
+        // Non-adjacent swapped nodes
+        if (first && last)
+            swap(first->val, last->val);
+
+        // Adjacent swapped nodes
+        else if (first && middle)
+            swap(first->val, middle->val);
     }
 };
