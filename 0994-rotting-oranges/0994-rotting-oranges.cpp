e63d823d40c00d6111487ce8d10eafb003e9ef59
Lovish Goyal <goyallovish852@gmail.com>
2026-06-22 16:29:15 +0530

    Time: 0 ms (100%), Space: 17.2 MB (29.52%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
+      int bfs(vector<vector<int>>& grid,queue<pair<int,pair<int,int>>>& q,vector<vector<int>>&visited,int cntFresh) {
+        int n = grid.size();
+        int m =grid[0].size();
+        int dr[4] = {-1,0,1,0};
+        int dc[4] = {0,-1,0,1};
+        int ans=0,cnt=0;
+        while(!q.empty()){
+            auto it = q.front();   q.pop();
+            int r=it.first;
+            int c=it.second.first;
+            int t=it.second.second;
+              for(int i=0;i<4;i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && grid[nr][nc]==1){
+                   visited[nr][nc]=1;
+                   q.push({nr,{nc,t+1}});
+                   grid[nr][nc]=2;
+                   cnt++; 
+                }
+              }
+            ans=t;
+        }
+        return cnt==cntFresh?ans:-1;
+    }
     int orangesRotting(vector<vector<int>>& grid) {
-       
-       int m = grid.size();
-       int n = grid[0].size();
-       vector<vector<int>> visited(m, vector<int>(n, 0));
-       queue<pair<pair<int,int>,int>>q;
-       int cnt = 0;
-       for(int i=0;i<m;i++){
-         for(int j=0;j<n;j++){
-            visited[i][j]=grid[i][j];
-            if(grid[i][j]==2) q.push({{i,j},0});
-            else if(grid[i][j]==1) cnt++;
-         }
-       }
-       int count=0;
-       int time=0;
-       int cols[] = {-1,0,1,0};
-       int rows[] = {0,-1,0,1};
-
-       while(!q.empty()){
-        int r = q.front().first.first;
-        int c = q.front().first.second;
-        int t = q.front().second;
-        time = max(t,time);
-        q.pop();
-        for(int i=0;i<4;i++){
-            int rn = r+rows[i];
-            int cn = c+cols[i];
-            if(rn >= 0 && rn < m && cn >= 0 && cn < n && visited[rn][cn] == 1){
-                q.push({{rn,cn},t+1});
-                visited[rn][cn]=2;
-                count++;
+        int n=grid.size();
+        int m=grid[0].size();
+        int cnt=0,cntFresh=0;
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        queue<pair<int,pair<int,int>>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                   q.push({i,{j,0}});
+                   visited[i][j]=1;
+                }
+                if(grid[i][j]==1) cntFresh+=1;
             }
         }
-       } 
-      if(cnt!=count) return -1;
-      return time;
+        cnt  = bfs(grid,q,visited,cntFresh);
+        return cnt;
     }
 };
