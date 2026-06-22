3c2d3193147a6d8d853915d11cf0e8dc716b6b4f
Lovish Goyal <goyallovish852@gmail.com>
2026-06-22 13:40:33 +0530

    Time: 34 ms (17.83%), Space: 24.3 MB (20.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,60 @@
 class Solution {
-public:
+private:
+    void bfs(int row, int col, vector<vector<int>>& vis,
+             vector<vector<char>>& grid) {
 
-    void bfs(vector<vector<char>>& grid,vector<vector<int>>& visited,int row,int col){
-        visited[row][col]=1;
-        int m = grid.size();
-        int n = grid[0].size();        
+        vis[row][col] = 1;
 
-        queue<pair<int,int>>q;
-        q.push({row,col});
+        queue<pair<int,int>> q;
+        q.push({row, col});
 
-        int delrow[] = {0,-1,0,1};
-        int delcol[] = {-1,0,1,0};
+        int n = grid.size();
+        int m = grid[0].size();
 
-        while(!q.empty()){
-          int rn = q.front().first;  
-          int cn = q.front().second;
-          q.pop();
-          for(int i=0;i<4;i++){
-            int r = rn+delrow[i];
-            int c = cn+delcol[i];
-            if(r>=0 && r<m && c>=0 && c<n && !visited[r][c] && grid[r][c]=='1'){
-                q.push({r,c});
-                visited[r][c]=1;
-            }
-          }
-        }  
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            int drow[] = {-1, 0, 1, 0};
+int dcol[] = {0, 1, 0, -1};
+
+for(int i = 0; i < 4; i++) {
+    int nrow = row + drow[i];
+    int ncol = col + dcol[i];
+
+    if(nrow >= 0 && nrow < n &&
+       ncol >= 0 && ncol < m &&
+       grid[nrow][ncol] == '1' &&
+       !vis[nrow][ncol]) {
+
+        vis[nrow][ncol] = 1;
+        q.push({nrow, ncol});
+    }
+}
+        }
     }
 
+public:
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>>visited(m,vector<int>(n,0));
-        int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        int cnt = 0;
+
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+
+                if(!vis[row][col] && grid[row][col] == '1') {
                     cnt++;
-                    bfs(grid,visited,i,j);
-                } 
+                    bfs(row, col, vis, grid);
+                }
             }
         }
+
         return cnt;
     }
 };
