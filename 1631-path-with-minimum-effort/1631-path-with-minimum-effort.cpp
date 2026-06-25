990b3dd24aa9af839f56b11d2c5699cc1d1c2984
Lovish Goyal <goyallovish852@gmail.com>
2026-06-25 12:19:11 +0530

    Time: 55 ms (58.06%), Space: 24.5 MB (77.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-         pq.push({0,{0,0}});
+
         int n = heights.size();
-        int m = heights[0].size(); 
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX)); 
+        int m = heights[0].size();
+
+        priority_queue<
+            pair<int,pair<int,int>>,
+            vector<pair<int,pair<int,int>>>,
+            greater<pair<int,pair<int,int>>>> pq;
+
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+
         dist[0][0] = 0;
+        pq.push({0,{0,0}});
+
         int dr[4] = {1,0,-1,0};
         int dc[4] = {0,1,0,-1};
-        while(!pq.empty()){
+
+        while(!pq.empty()) {
+
             int e = pq.top().first;
             int r = pq.top().second.first;
             int c = pq.top().second.second;
             pq.pop();
-             for(int i=0;i<4;i++){
+
+            if(r == n-1 && c == m-1)
+                return e;
+
+            for(int i=0;i<4;i++) {
+
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && dist[nr][nc]> abs(heights[r][c]-heights[nr][nc])
-                  && dist[nr][nc]> e){
-                    int ne = max(e,abs(heights[r][c]-heights[nr][nc]));
-                    pq.push({ne,{nr,nc}});
-                    dist[nr][nc]=ne;
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m) {
+
+                    int ne = max(e,
+                                 abs(heights[r][c]-heights[nr][nc]));
+
+                    if(ne < dist[nr][nc]) {
+
+                        dist[nr][nc] = ne;
+                        pq.push({ne,{nr,nc}});
+                    }
                 }
-             }
-        } 
-        return dist[n-1][m-1];   
+            }
+        }
+
+        return 0;
     }
 };
