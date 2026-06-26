883032c93108e408f966d19f2e988d3e5adec6ba
Lovish Goyal <goyallovish852@gmail.com>
2026-06-26 14:04:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -32,28 +32,13 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         DisjointSets ds(n);
-        vector<pair<int,int>>edges;
-        // for(int i=0;i<n;i++){
-        //  // int v=1;  
-        //   for(auto it :isConnected[i]){
-        //     int u=i+1;
-        //     if(it==1) edges.push_back({u,v});
-        //     v++;
-        //   } 
-        // }
-         for(int i=0;i<n;i++){  
+
+        for(int i=0;i<n;i++){  
           for(int j=0;j<n;j++){
             if(isConnected[i][j]) ds.unionBysize(i,j);
           } 
         }
-       // sort(edges.begin(),edges.end());
-        // for(auto it:edges){
-        //     int u = it.first;
-        //     int v = it.second;
-        //     if(ds.findUpar(u)!=ds.findUpar(v)){
-        //         ds.unionBysize(u,v);
-        //     }
-        // }
+    
         int cnt=0;
         for(int i=0;i<n;i++){
             if(ds.findUpar(i)==i) cnt++;
