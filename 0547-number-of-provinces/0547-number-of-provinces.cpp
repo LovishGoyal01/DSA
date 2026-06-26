fa332f01cc80adbe78aef9a2d55b178f6c2f7846
Lovish Goyal <goyallovish852@gmail.com>
2026-06-26 14:11:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.93%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -46,3 +46,36 @@ public:
         return cnt;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n= isConnected.size();
+//         vector<bool>vis(n,false);
+//         int cnt=0;
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                   calc(isConnected,vis,i);
+//                   cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     void calc(vector<vector<int>>& isConnected,vector<bool>& vis,int node){
+//        int n=isConnected.size();
+//         vis[node]=true;
+//         queue<int>q;
+//         q.push(node);
+//         while(!q.empty()){
+//             int root=q.front(); q.pop();
+//             for(int i=0;i<n;i++){
+//                 if(!vis[i] && isConnected[root][i]){
+//                     q.push(i);
+//                     vis[i]=true;
+//                 }
+//             }
+//         }
+//     }
+// };
