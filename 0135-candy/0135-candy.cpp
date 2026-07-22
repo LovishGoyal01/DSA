cb5aa35fffcb35424c07b61632026572a7664dce
Lovish Goyal <goyallovish852@gmail.com>
2026-07-22 18:11:37 +0530

    Time: 0 ms (100%), Space: 22.9 MB (52.66%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-       int n=ratings.size();
-       int i=1,sum=1;
-       while(i<n){
-          if(ratings[i]==ratings[i-1]){
-            sum+=1;
-            i++;
-            continue;
-          }
-          int peak=1;
-          while(i<n && ratings[i]>ratings[i-1]){
-            peak++;
-            sum+=peak;
-            i++;
-          }
-          int down=1;
-          while(i<n && ratings[i]<ratings[i-1]){
-            sum+=down;
-            down++;
-            i++;
-          } 
-          if(down>peak) sum+=down-peak;
-       }
-       return sum;
+        int n=ratings.size();
+        vector<int>arr(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]) arr[i]=arr[i-1]+1;
+            else arr[i]=1;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) arr[i]=max(arr[i],arr[i+1]+1);
+        }
+        int sum = accumulate(arr.begin(),arr.end(),0);
+        return sum;
     }
 };
