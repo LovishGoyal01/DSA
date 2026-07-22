14b390e1e2e8fec9c90bd0c417ff4e46788ea1ff
Lovish Goyal <goyallovish852@gmail.com>
2026-07-22 17:02:21 +0530

    Time: 0 ms (100%), Space: 21.8 MB (48.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,19 +5,22 @@ public:
         int n=intervals.size();
         int i=0;
         while(i<n && newInterval[0]> intervals[i][1]){
-            res.push_back(intervals[i]); 
-            i++;
+           res.push_back(intervals[i]);
+           i++;
         }
+
         while(i<n && newInterval[1]>=intervals[i][0]){
-            newInterval[0]=min(newInterval[0],intervals[i][0]);
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            newInterval[0] = min(newInterval[0],intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1]);
             i++;
         }
         res.push_back(newInterval);
         while(i<n){
-            res.push_back(intervals[i]);
-            i++;
+             res.push_back(intervals[i]);
+             i++;
         }
+        return res;
+       
       return res;
     }
 };
