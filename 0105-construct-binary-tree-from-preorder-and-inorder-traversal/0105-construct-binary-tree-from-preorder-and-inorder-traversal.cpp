d9dbd1cbe72bde259eaed3b7e977ad8b8457a9ae
Lovish Goyal <goyallovish852@gmail.com>
2026-07-23 22:06:23 +0530

    Time: 0 ms (100%), Space: 27.6 MB (33.92%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,45 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int, int> inMap;
-
-        for (int i = 0; i < inorder.size(); i++) {
-            inMap[inorder[i]] = i;
+        map<int,int>mp;
+        int n=inorder.size();
+        for(int i=0;i<n;i++){
+            mp[inorder[i]]=i;
         }
-
-        return build(preorder, 0, preorder.size() - 1,
-                     inorder, 0, inorder.size() - 1,
-                     inMap);
+        return build(preorder,0,n-1,inorder,0,n-1,mp);
     }
+    
+    TreeNode* build(vector<int>& preorder,int prestart,int preend,vector<int>& inorder,int instart,int inend,map<int,int>& mp){
+        if(prestart>preend || instart>inend) return nullptr;
+        TreeNode* node = new TreeNode(preorder[prestart]);
+        int inroot = mp[node->val];
+        int numsonLeft = inroot-instart;
 
-    TreeNode* build(vector<int>& preorder, int preStart, int preEnd,
-                    vector<int>& inorder, int inStart, int inEnd,
-                    map<int, int>& inMap) {
+        node->left = build(preorder,prestart+1,prestart+numsonLeft,
+                           inorder,instart,inroot-1,mp);
 
-        if (preStart > preEnd || inStart > inEnd)
-            return nullptr;
-
-        TreeNode* root = new TreeNode(preorder[preStart]);
-
-        int inRoot = inMap[root->val];
-        int numsLeft = inRoot - inStart;
-
-        root->left = build(preorder,
-                           preStart + 1,
-                           preStart + numsLeft,
-                           inorder,
-                           inStart,
-                           inRoot - 1,
-                           inMap);
-
-        root->right = build(preorder,
-                            preStart + numsLeft + 1,
-                            preEnd,
-                            inorder,
-                            inRoot + 1,
-                            inEnd,
-                            inMap);
-
-        return root;
+        node->right = build(preorder,prestart+numsonLeft+1,preend,
+                           inorder,inroot+1,inend,mp); 
+        return node;                                     
     }
 };
