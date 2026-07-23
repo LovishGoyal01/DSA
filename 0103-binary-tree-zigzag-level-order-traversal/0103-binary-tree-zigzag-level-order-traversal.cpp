316e884fa421dd8dbfa03aa7f104b10a732fdb10
Lovish Goyal <goyallovish852@gmail.com>
2026-07-23 13:59:58 +0530

    Time: 0 ms (100%), Space: 15.1 MB (48.67%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,38 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-
-        if (root == nullptr)
-            return result;
-
-        queue<TreeNode*> nodesQueue;
-        nodesQueue.push(root);
-
-        bool leftToRight = true;
-
-        while (!nodesQueue.empty()) {
-            int size = nodesQueue.size();
-            vector<int> row(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = nodesQueue.front();
-                nodesQueue.pop();
-
-                int index = leftToRight ? i : size - 1 - i;
-                row[index] = node->val;
-
-                if (node->left)
-                    nodesQueue.push(node->left);
-
-                if (node->right)
-                    nodesQueue.push(node->right);
+        queue<TreeNode*>q;
+        bool LeftToRight = true;
+        vector<vector<int>>ans;
+        if(root==nullptr) return ans;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> arr(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                int index = LeftToRight ? i : size-i-1;
+                arr[index] = node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                
             }
-
-            leftToRight = !leftToRight;
-            result.push_back(row);
+            ans.push_back(arr);
+            LeftToRight = !LeftToRight;
         }
-
-        return result;
+        return ans;
     }
 };
