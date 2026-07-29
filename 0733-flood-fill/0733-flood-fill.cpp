5e01b3b8bffaaf90cf079a257f228180005c5ac5
Lovish Goyal <goyallovish852@gmail.com>
2026-07-29 19:39:00 +0530

    Time: 0 ms (100%), Space: 18.4 MB (31.34%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m =image[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
         queue<pair<int,int>>q;
         q.push({sr,sc});
+        int m=image.size();
+        int n= image[0].size();
+        
+        int dr[4] = {-1,0,1,0};
+        int dc[4] = {0,-1,0,1};
+        
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
         visited[sr][sc]=1;
+        int prevColor=image[sr][sc];
+        image[sr][sc] = color;
+        
         while(!q.empty()){
-            auto it = q.front();   q.pop();
-            int dr[4] = {-1,0,1,0};
-            int dc[4] = {0,-1,0,1};
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
             for(int i=0;i<4;i++){
-                int nr = it.first + dr[i];
-                int nc = it.second + dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && image[nr][nc]==image[sr][sc]){
-                   visited[nr][nc]=1;
-                   q.push({nr,nc});
-                   image[nr][nc]=color; 
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc]==prevColor && !visited[nr][nc] ){
+                    visited[nr][nc]=1;
+                    q.push({nr,nc});
+                    image[nr][nc]=color;
                 }
             }
         }
-          image[sr][sc]=color;
         return image;
     }
 };
