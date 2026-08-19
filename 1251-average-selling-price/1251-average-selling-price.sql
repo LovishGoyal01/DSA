910cbbbdbd9c7c2e0d9511f20aefafbd426cc093
Lovish Goyal <goyallovish852@gmail.com>
2026-08-19 11:17:26 +0530

    Time: 809 ms (78.87%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,8 +1,8 @@
 WITH cte AS (
     SELECT 
         p.product_id,
-        p.price * COALESCE(u.units, 0) AS mult,
-        COALESCE(u.units, 0) AS uni
+        (u.units * p.price) AS mult,
+        u.units AS uni
     FROM Prices p
     LEFT JOIN UnitsSold u
         ON p.product_id = u.product_id
@@ -11,6 +11,6 @@ WITH cte AS (
 
 SELECT 
     product_id,
-    COALESCE(ROUND(SUM(mult) / NULLIF(SUM(uni), 0), 2), 0) AS average_price
+    COALESCE(ROUND(SUM(mult) / SUM(uni), 2), 0) AS average_price
 FROM cte
 GROUP BY product_id;
