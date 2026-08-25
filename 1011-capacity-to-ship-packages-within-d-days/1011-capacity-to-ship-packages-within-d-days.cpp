44f14079d6024c634e966eede4515ab13f5af9e1
Lovish Goyal <goyallovish852@gmail.com>
2026-08-25 14:02:06 +0530

    Time: 11 ms (65.12%), Space: 35 MB (88.66%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int calc(vector<int>& arr,int mid, int days){
-        int total=0,count=1;
-        for(int i=0;i<arr.size();i++){
-            if(total+arr[i]>mid){
-                count++;
-                total=arr[i];
-            }else total+=arr[i];
+
+    int fxn(vector<int>& weights, int mid, int days){
+        int sum=0,cnt=0;
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            if(sum>mid){
+                cnt++;
+                sum=weights[i];
+            }
         }
-        if(count<=days) return 1;
+        if(cnt+1<=days) return 1;
         return 0;
     }
 
-    int shipWithinDays(vector<int>& arr, int days) {
-        int n=arr.size();
-        int low = *max_element(arr.begin(), arr.end());
-        int high=accumulate(arr.begin(),arr.end(),0);
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
         int mid,ans=-1;
         while(low<=high){
-            mid=(low+high)/2;
-            if(calc(arr,mid,days)==1){
+            mid =(low+high)/2;
+            if(fxn(weights,mid,days)==1){
                 high=mid-1;
                 ans=mid;
             }else low=mid+1;
