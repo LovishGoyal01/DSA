0863c70dc14093a2bb2fecadd14e59e0e79e0c2a
Lovish Goyal <goyallovish852@gmail.com>
2026-08-27 13:47:19 +0530

    Time: 1 ms (63.43%), Space: 14.1 MB (58.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void rec(vector<int>& candidates, int index, int target,vector<vector<int>>& ans,vector<int> arr){
-        if(index==candidates.size()){
-            if(target==0){
-                ans.push_back(arr);
-            }
+    void rec(vector<int>& candidates, int target,vector<int>& array,vector<vector<int>>& ans,int s){
+        if(target==0){
+            ans.push_back(array);
             return;
         }
-        if(candidates[index]<=target){
-            arr.push_back(candidates[index]);
-            rec(candidates,index,target-candidates[index],ans,arr);
-            arr.pop_back(); 
+
+        for(int i=s;i<candidates.size();i++){
+            if(target - candidates[i]>=0){
+             array.push_back(candidates[i]);
+             rec(candidates,target- candidates[i],array,ans,i);
+             array.pop_back();
+            }
         }
-        rec(candidates,index+1,target,ans,arr);
     }
- 
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int> arr;
-        rec(candidates,0,target,ans,arr);
+        vector<vector<int>> ans;
+        vector<int> array;
+        rec(candidates,target,array,ans,0);
         return ans;
     }
 };
