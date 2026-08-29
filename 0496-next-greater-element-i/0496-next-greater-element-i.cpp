358332d81ec233c37bdfa3276a5bb01620d7e0f3
Lovish Goyal <goyallovish852@gmail.com>
2026-08-29 11:11:38 +0530

    Time: 4 ms (23.07%), Space: 12.3 MB (92.51%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>st;
-        int n = nums2.size();
-        vector<int>ans(n,-1);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top()<=nums2[i]) st.pop();
-            ans[i] = st.empty() ? -1 : st.top();
-            st.push(nums2[i]);
+     stack<int>st;
+     int n=nums2.size();
+     vector<int> ans(nums1.size(),-1);
+     int i=n-1,val;
+     while(i>=0){
+        while(!st.empty() && st.top()<=nums2[i]) st.pop();
+        if(st.empty()) val=-1;
+        else val=st.top();
+        st.push(nums2[i]);
+        for(int j=0;j<nums1.size();j++){
+            if(nums2[i]==nums1[j]) ans[j]=val;
         }
-        vector<int>arr(nums1.size(),-1);
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<n;j++){
-                if(nums1[i]==nums2[j]){
-                    arr[i]=ans[j];
-                    break;
-                }
-            }                         
-        }
-      return arr;
+        i--;
+     } 
+     return ans;  
     }
 };
