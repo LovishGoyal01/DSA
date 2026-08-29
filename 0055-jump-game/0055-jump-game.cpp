9704bd59e84ffadb78775f16650ed15f0e8def58
Lovish Goyal <goyallovish852@gmail.com>
2026-08-29 22:45:32 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxI=0;
-        for(int i=0;i<nums.size();i++){
-            if(i>maxI) return false;
-            maxI=max(nums[i]+i,maxI);
-        }
-        return true;
+      int r=0;
+      for(int i=0;i<nums.size();i++){
+        if(i>r) return false;
+        r=max(r,i+nums[i]);
+      }  
+      return true;
     }
 };
